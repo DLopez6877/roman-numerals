a022e8ad770b0a77e57ae64b9db4de5fc5cc07af
Dylan Dills and Daniel Lopez <daniel@>
2017-05-04 14:01:01 -0700

    convert single number into a roman numeral

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 | Behavior: this program | Inputs Example: when it receives | Output: it should return|
 |------------------|:-------------:|------:|
-|Converts number 1 into "I"|1|I|
-|Converts number 2 into "II"|2|II|
+|Convert a single number into a roman numeral|1|I|
+|Convert two or more numbers into a roman numeral|12|XII|
+<!-- |Converts number 2 into "II"|2|II|
 |Converts number 3 into "III"|3|III|
 |Converts number 4 into "IV"|1|I|
 |Converts number 5 into "V"|2|II|
 |Converts number 6 into "VI"|3|III|
 |Converts number 7 into "VII"|1|I|
 |Converts number 8 into "VIII"|2|II|
-|Converts number 9 into "IX"|3|III|
+|Converts number 9 into "IX"|3|III| -->
--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,47 @@
-var numbersZeroNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var numbersOneNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var romansOneNine = ["0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
 
-var convert = function (userInput) {
-  // converts number 1 into "1"
-  if (userInput === 1) {
-    return "I";
-    // converts number 2 into "II"
-  } else if (userInput === 2) {
-    return "II";
-    // converts number 3 into "III"
-  } else if (userInput === 3) {
-    return "III";
-    // converts number 4 into "IV"
-  } else if (userInput === 4) {
-    return "IV";
-    // converts number 5 into "V"
-  } else if (userInput === 5) {
-    return "V";
-    // converts number 6 into "VI"
-  } else if (userInput === 6) {
-    return "VI";
-    // converts number 7 into "VII"
-  } else if (userInput === 7) {
-    return "VII";
-    // converts number 8 into "VIII"
-  } else if (userInput === 8) {
-    return "VIII";
-    // converts number 9 into "IX"
-  } else if (userInput === 9) {
-    return "IX";
-  } else {
-    return "";
+// convert single number into a roman numberal
+var convert = function(userInput) {
+  for(var index = 0; index < numbersOneNine.length; index += 1) {
+    if (userInput === index) {
+      userInput = romansOneNine[index];
+      return userInput;
+    }
   }
-};
+}
+// var convert = function (userInput) {
+//   // converts number 1 into "1"
+//   if (userInput === 1) {
+//     return "I";
+//     // converts number 2 into "II"
+//   } else if (userInput === 2) {
+//     return "II";
+//     // converts number 3 into "III"
+//   } else if (userInput === 3) {
+//     return "III";
+//     // converts number 4 into "IV"
+//   } else if (userInput === 4) {
+//     return "IV";
+//     // converts number 5 into "V"
+//   } else if (userInput === 5) {
+//     return "V";
+//     // converts number 6 into "VI"
+//   } else if (userInput === 6) {
+//     return "VI";
+//     // converts number 7 into "VII"
+//   } else if (userInput === 7) {
+//     return "VII";
+//     // converts number 8 into "VIII"
+//   } else if (userInput === 8) {
+//     return "VIII";
+//     // converts number 9 into "IX"
+//   } else if (userInput === 9) {
+//     return "IX";
+//   } else {
+//     return "";
+//   }
+// };
 
 
 
@@ -47,6 +57,5 @@ $(document).ready(function() {
     var userInput = parseInt($("input#numberInput").val());
     var result = romanCalc(userInput);
     $("#result").text(result);
-    console.log(result);
   });
 });
