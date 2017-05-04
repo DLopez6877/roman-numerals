1c88b3a6f8034f227b26c401ec9b4ea19e903fc1
Dylan Dills and Daniel Lopez <dylan@>
2017-05-04 16:03:22 -0700

    convert a two digit and three digit number into a roman numeral

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
 | Behavior: this program | Inputs Example: when it receives | Output: it should return|
 |------------------|:-------------:|------:|
 |Convert a single number into a roman numeral|1|I|
-|Convert two or more numbers into a roman numeral|12|XII|
-<!-- |Converts number 2 into "II"|2|II|
-|Converts number 3 into "III"|3|III|
-|Converts number 4 into "IV"|1|I|
-|Converts number 5 into "V"|2|II|
-|Converts number 6 into "VI"|3|III|
-|Converts number 7 into "VII"|1|I|
-|Converts number 8 into "VIII"|2|II|
-|Converts number 9 into "IX"|3|III| -->
+|Convert a two digit number into a roman numeral|12|XII|
+|Convert a three digit number into a roman numeral|134|CXXXIV|
+|Convert a four digit number into a roman numeral|12|XII|
--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,62 @@
-var numbersOneNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var romansOneNine = ["0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+var numbersOneNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// convert single number into a roman numberal
-var convert = function(userInput) {
+// convert single number into a roman numeral
+var convertOneDigit = function(userInput) {
+  var newArray = [];
+  var numArray = userInput.toString().split("");
+
+
+  var oneDigitRoman = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
   for(var index = 0; index < numbersOneNine.length; index += 1) {
-    if (userInput === index) {
-      userInput = romansOneNine[index];
-      return userInput;
+    if (parseInt(numArray[numArray.length-1]) === index) {
+      newArray.push(oneDigitRoman[index]);
     }
   }
+  return newArray.toString();
+
 }
-// var convert = function (userInput) {
-//   // converts number 1 into "1"
-//   if (userInput === 1) {
-//     return "I";
-//     // converts number 2 into "II"
-//   } else if (userInput === 2) {
-//     return "II";
-//     // converts number 3 into "III"
-//   } else if (userInput === 3) {
-//     return "III";
-//     // converts number 4 into "IV"
-//   } else if (userInput === 4) {
-//     return "IV";
-//     // converts number 5 into "V"
-//   } else if (userInput === 5) {
-//     return "V";
-//     // converts number 6 into "VI"
-//   } else if (userInput === 6) {
-//     return "VI";
-//     // converts number 7 into "VII"
-//   } else if (userInput === 7) {
-//     return "VII";
-//     // converts number 8 into "VIII"
-//   } else if (userInput === 8) {
-//     return "VIII";
-//     // converts number 9 into "IX"
-//   } else if (userInput === 9) {
-//     return "IX";
-//   } else {
-//     return "";
-//   }
-// };
 
 
+// Convert a two digit number into a roman numeral
+var convertTwoDigit = function(userInput) {
+  var twoDigitRoman = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+  var newArray = [];
+  var numArray = userInput.toString().split("");
 
-// final calculator function
+
+  for(var index = 0; index < numbersOneNine.length; index += 1) {
+    if (parseInt(numArray[numArray.length-2]) === index) {
+      newArray.push(twoDigitRoman[index]);
+    }
+  }
+  return newArray.toString();
+}
+
+// Convert a three digit number into a roman numeral
+var convertThreeDigit = function(userInput) {
+  var threeDigitRoman = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+  var newArray = [];
+  var numArray = userInput.toString().split("");
+
+
+  for(var index = 0; index < numbersOneNine.length; index += 1) {
+    if (parseInt(numArray[numArray.length-3]) === index) {
+      newArray.push(threeDigitRoman[index]);
+    }
+  }
+  return newArray.toString();
+}
+
 var romanCalc = function(userInput) {
-  return convert(userInput);
+  var newArray = [];
+
+  var oneDigit = convertOneDigit(userInput); //
+  var twoDigit = convertTwoDigit(userInput);
+  var threeDigit = convertThreeDigit(userInput);
+
+  newArray.push(threeDigit + twoDigit + oneDigit);
+
+  return newArray.toString(); // 12
 };
 
 // User Interface goes here
